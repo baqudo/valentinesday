e324b58f4d6ce06b67c07654a0075137b68ea78e
BaQudo <baqudo@gmail.com>
2017-02-03 17:45:01 +0200

    comit
    cc

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,14 @@
+var gulp   = require('gulp');
+var config = require('../config');
+
+gulp.task('watch', 
+    ['copy:watch',
+    
+    'nunjucks:watch',
+    'iconfont:watch',
+    'sprite:svg:watch',
+    'sprite:png:watch',
+    'svgo:watch',
+    'js:watch',
+    'sass:watch'
+]);
